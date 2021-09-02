685db2303326ba932fac61cb3b9af1c817731417
master <root@root@com>
2021-09-02 19:49:00 +0300

    change datetime field from auto_new to auto_new_add

--- polls/models.py
+++ polls/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 class Poll(models.Model):
     question = models.CharField(max_length=100)
     created_by = models.ForeignKey(User, on_delete=models.CASCADE)
-    pub_date = models.DateTimeField(auto_now=True)
+    pub_date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.question
