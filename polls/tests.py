5a73eccb7273805fb7286edc0fc5403457f70f85
master <root@root@com>
2021-09-06 00:06:06 +0300

    same test with APITestCase

--- polls/tests.py
+++ polls/tests.py
@@ -1,19 +1,42 @@
 from django.contrib.auth import get_user_model
 from rest_framework.authtoken.models import Token
-from rest_framework.test import APITestCase
+from rest_framework.test import APITestCase, APIClient
 from rest_framework.test import APIRequestFactory
 
 from polls import apiviews
 
 
+# class TestPoll(APITestCase):
+#     def setUp(self):
+#         self.factory = APIRequestFactory()
+#         self.view = apiviews.PollViewSet.as_view({'get': 'list'})
+#         self.uri = '/polls/'
+#         self.user = self.setup_user()
+#         self.token = Token.objects.create(user=self.user)
+#         self.token.save()
+#
+#     @staticmethod
+#     def setup_user():
+#         User = get_user_model()
+#         return User.objects.create_user(
+#             'test',
+#             email='testuser@test.com',
+#             password='test'
+#         )
+#
+#     def test_list(self):
+#         request = self.factory.get(self.uri, HTTP_AUTHORIZATION=f'Token {self.token.key}')
+#         response = self.view(request)
+#         self.assertEqual(response.status_code, 200,
+#                          f'Expected Response Code 200, received {response.status_code} instead.')
+
+
+# ==== The same test can be written using APIClient ====================================
 class TestPoll(APITestCase):
     def setUp(self):
-        self.factory = APIRequestFactory()
-        self.view = apiviews.PollViewSet.as_view({'get': 'list'})
+        self.client = APIClient()
         self.uri = '/polls/'
         self.user = self.setup_user()
-        self.token = Token.objects.create(user=self.user)
-        self.token.save()
 
     @staticmethod
     def setup_user():
@@ -24,9 +47,9 @@ class TestPoll(APITestCase):
             password='test'
         )
 
-    def test_list(self):
-        request = self.factory.get(self.uri, HTTP_AUTHORIZATION=f'Token {self.token.key}')
-        response = self.view(request)
+    def test_list2(self):
+        self.client.login(username="test", password="test")
+        response = self.client.get(self.uri)
         self.assertEqual(response.status_code, 200,
                          f'Expected Response Code 200, received {response.status_code} instead.')
 
