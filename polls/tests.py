261091e119595b8ddd9ae7823aa4661be8d766ef
master <root@root@com>
2021-09-05 23:44:56 +0300

    testing APIs with authentication

--- polls/tests.py
+++ polls/tests.py
@@ -1,3 +1,32 @@
-from django.test import TestCase
+from django.contrib.auth import get_user_model
+from rest_framework.authtoken.models import Token
+from rest_framework.test import APITestCase
+from rest_framework.test import APIRequestFactory
 
-# Create your tests here.
+from polls import apiviews
+
+
+class TestPoll(APITestCase):
+    def setUp(self):
+        self.factory = APIRequestFactory()
+        self.view = apiviews.PollViewSet.as_view({'get': 'list'})
+        self.uri = '/polls/'
+        self.user = self.setup_user()
+        self.token = Token.objects.create(user=self.user)
+        self.token.save()
+
+    @staticmethod
+    def setup_user():
+        User = get_user_model()
+        return User.objects.create_user(
+            'test',
+            email='testuser@test.com',
+            password='test'
+        )
+
+    def test_list(self):
+        request = self.factory.get(self.uri, HTTP_AUTHORIZATION=f'Token {self.token.key}')
+        response = self.view(request)
+        self.assertEqual(response.status_code, 200,
+                         f'Expected Response Code 200, received {response.status_code} instead.')
+
